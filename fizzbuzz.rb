3635d8a2ef0888fea9fd447f7a807c724e691c3e
Elizabeth Jimenez <lizliz123@gmail.com>
2018-03-26 19:41:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)  
-if int.to_i % 3 == 0 
+def fizzbuzz(int) do |i| 
+if i % 3 == 0 
   puts "Fizz"
-elsif int % 5 == 0 
+elsif i % 5 == 0 
   puts "Buzz"
-elsif int % 3 == 0 && int % 5 == 0
+elsif i % 3 == 0 && int % 5 == 0
   puts "FizzBuzz"
 else
   puts "nil"
